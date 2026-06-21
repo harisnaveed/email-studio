576f1fd55e176ba851c0ae803c5ef7e06cf375c5
Haris Naveed <harisnaved5101996@gmail.com>
2026-06-22 01:10:24 +0500

    icons allignment

--- src/utils/emailGenerator.js
+++ src/utils/emailGenerator.js
@@ -346,59 +346,70 @@ export function generateEmailHTML(blocks, globalSettings) {
           const activePlatforms = platforms.filter(p => p.enabled);
           if (activePlatforms.length === 0) return '';
 
-          const getColors = (name) => {
-            let bg = 'transparent';
-            let fg = '7c3aed';
-
-            if (colorTheme === 'original') {
-              if (name === 'facebook') bg = '1877F2';
-              else if (name === 'twitter') bg = '0f1419';
-              else if (name === 'instagram') bg = 'E4405F';
-              else if (name === 'linkedin') bg = '0A66C2';
-              else if (name === 'youtube') bg = 'FF0000';
-            } else if (colorTheme === 'dark') {
-              bg = '334155';
-              fg = bg;
-            } else if (colorTheme === 'light') {
-              bg = 'e2e8f0';
-              fg = '475569';
-            } else {
-              bg = customColor.replace('#', '');
-              fg = bg;
-            }
-
-            return { bg: '#' + bg, fg };
+          // Brand colors
+          const BRAND_COLORS = {
+            facebook:  '#1877F2',
+            twitter:   '#0f1419',
+            instagram: '#E4405F',
+            linkedin:  '#0A66C2',
+            youtube:   '#FF0000',
           };
+
+          // Inline SVG path data (viewBox 0 0 24 24)
+          const SVG_PATHS = {
+            facebook:  'M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95C18.05 21.45 22 17.19 22 12z',
+            twitter:   'M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.73-8.835L2.25 2.25h6.894l4.383 5.794L18.244 2.25zm-1.161 17.52h1.833L7.084 4.126H5.117L17.083 19.77z',
+            instagram: 'M12 2c2.717 0 3.056.01 4.122.06 1.065.05 1.79.217 2.428.465.66.254 1.216.598 1.772 1.153.509.5.902 1.105 1.153 1.772.247.637.415 1.363.465 2.428.047 1.066.06 1.405.06 4.122s-.013 3.056-.06 4.122c-.05 1.065-.218 1.79-.465 2.428a4.883 4.883 0 01-1.153 1.772 4.915 4.915 0 01-1.772 1.153c-.637.247-1.363.415-2.428.465-1.066.047-1.405.06-4.122.06s-3.056-.013-4.122-.06c-1.065-.05-1.79-.218-2.428-.465a4.89 4.89 0 01-1.772-1.153 4.904 4.904 0 01-1.153-1.772c-.248-.637-.415-1.363-.465-2.428C2.013 15.056 2 14.717 2 12c0-2.717.01-3.056.06-4.122.05-1.066.217-1.79.465-2.428a4.88 4.88 0 011.153-1.772A4.897 4.897 0 015.45 2.525c.638-.248 1.362-.415 2.428-.465C8.944 2.013 9.283 2 12 2zm0 1.802c-2.67 0-2.986.01-4.04.059-.976.045-1.505.207-1.858.344-.466.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.048 1.055-.058 1.37-.058 4.041 0 2.67.01 2.986.058 4.04.045.976.207 1.505.344 1.858.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058 2.67 0 2.987-.01 4.04-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.054.058-1.37.058-4.041 0-2.67-.01-2.986-.058-4.04-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.054-.048-1.37-.058-4.041-.058zm0 3.063a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z',
+            linkedin:  'M19 3a2 2 0 012 2v14a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h14zm-.5 15.5v-5.3a3.26 3.26 0 00-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 011.4 1.4v4.93h2.79zM6.88 8.56a1.68 1.68 0 001.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 00-1.69 1.69c0 .93.76 1.68 1.69 1.68zm1.39 9.94v-8.37H5.5v8.37h2.77z',
+            youtube:   'M21.543 6.498C22 8.28 22 12 22 12s0 3.72-.457 5.502c-.254.985-.997 1.76-1.938 2.022C17.896 20 12 20 12 20s-5.893 0-7.605-.476c-.945-.266-1.687-1.04-1.938-2.022C2 15.72 2 12 2 12s0-3.72.457-5.502c.254-.985.997-1.76 1.938-2.022C6.107 4 12 4 12 4s5.896 0 7.605.476c.945.266 1.687 1.04 1.938 2.022zM10 15.5l6-3.5-6-3.5v7z',
+          };
+
+          const getColors = (name) => {
+            let bg = customColor;
+            let fg = '#ffffff';
+            if (colorTheme === 'original') {
+              bg = BRAND_COLORS[name] || customColor;
+            } else if (colorTheme === 'dark') {
+              bg = '#334155';
+            } else if (colorTheme === 'light') {
+              bg = '#e2e8f0';
+              fg = '#334155';
+            }
+            if (iconStyle === 'plain') {
+              fg = colorTheme === 'original' ? (BRAND_COLORS[name] || customColor)
+                 : colorTheme === 'light' ? '#334155'
+                 : customColor;
+              bg = 'transparent';
+            }
+            return { bg, fg };
+          };
+
+          const cellDim = Math.round(iconSizeVal * 1.6);
+          const radius = iconStyle === 'circle' ? '50%' : iconStyle === 'square' ? '6px' : '0';
 
           const iconsHTML = activePlatforms.map((p, idx) => {
             const { bg, fg } = getColors(p.name);
-            
-            let icons8Name = p.name;
-            if (p.name === 'facebook') icons8Name = 'facebook-new';
-            else if (p.name === 'twitter') icons8Name = 'twitterx';
-            else if (p.name === 'instagram') icons8Name = 'instagram-new';
-            else if (p.name === 'youtube') icons8Name = 'youtube-play';
+            const svgPath = SVG_PATHS[p.name] || SVG_PATHS.twitter;
 
-            let imgUrl;
-            if (colorTheme === 'original') {
-              imgUrl = `https://img.icons8.com/color/${iconSizeVal * 2}/${icons8Name}.png`;
-            } else if (colorTheme === 'light') {
-              imgUrl = `https://img.icons8.com/ios-filled/${iconSizeVal * 2}/475569/${icons8Name}.png`;
-            } else {
-              imgUrl = `https://img.icons8.com/ios-filled/${iconSizeVal * 2}/${fg}/${icons8Name}.png`;
-            }
+            const separatorCell = idx > 0
+              ? `<td width="${spacingVal}" style="width: ${spacingVal}px; font-size: 1px; line-height: 1px;">&nbsp;</td>`
+              : '';
 
-            const cellDim = Math.round(iconSizeVal * 1.6);
-            const radius = iconStyle === 'circle' ? '50%' : iconStyle === 'square' ? '6px' : '0';
-            const cellBg = iconStyle === 'plain' ? 'transparent' : bg;
+            // Calculate padding to perfectly center the SVG icon inside the container
+            const iconPad = iconStyle === 'plain' ? 0 : Math.round((cellDim - iconSizeVal) / 2);
+            const wrapDim  = iconStyle === 'plain' ? iconSizeVal : cellDim;
 
-            const separatorCell = idx > 0 ? `<td width="${spacingVal}" style="width: ${spacingVal}px; font-size: 1px; line-height: 1px;">&nbsp;</td>` : '';
+            const cellStyle = iconStyle === 'plain'
+              ? `width: ${iconSizeVal}px; height: ${iconSizeVal}px; padding: 0; text-align: center; vertical-align: middle;`
+              : `background-color: ${bg}; border-radius: ${radius}; width: ${cellDim}px; height: ${cellDim}px; padding: 0; text-align: center; vertical-align: middle; mso-table-lspace:0pt; mso-table-rspace:0pt;`;
 
             return `
               ${separatorCell}
-              <td align="center" valign="middle" bgcolor="${cellBg}" style="background-color: ${cellBg}; border-radius: ${radius}; width: ${cellDim}px; height: ${cellDim}px; padding: 0; mso-table-lspace:0pt; mso-table-rspace:0pt;">
-                <a href="${p.url}" target="_blank" style="text-decoration: none; display: block; height: ${cellDim}px; width: ${cellDim}px; line-height: ${cellDim}px;">
-                  <img src="${imgUrl}" width="${iconSizeVal}" height="${iconSizeVal}" alt="${p.name}" style="display: inline-block; vertical-align: middle; border: 0; outline: none; text-decoration: none;" />
+              <td align="center" valign="middle" bgcolor="${iconStyle === 'plain' ? 'transparent' : bg}" style="${cellStyle}">
+                <a href="${p.url}" target="_blank" style="text-decoration: none; display: block; padding: ${iconPad}px; line-height: 0; font-size: 0; text-align: center;">
+                  <svg width="${iconSizeVal}" height="${iconSizeVal}" viewBox="0 0 24 24" fill="${fg}" xmlns="http://www.w3.org/2000/svg" style="display: block; border: 0; outline: none;">
+                    <path d="${svgPath}" />
+                  </svg>
                 </a>
               </td>
             `;
@@ -441,7 +452,7 @@ export function generateEmailHTML(blocks, globalSettings) {
                       <a href="${videoUrl}" target="_blank" style="text-decoration: none; display: block; border: 0; outline: none;">
                         <img src="${coverUrl}" width="${width.replace('px', '')}" style="display: block; width: 100%; border: 0; max-width: 100%; border-radius: 6px;" alt="Play video" />
                         <span style="position: absolute; top: 50%; left: 50%; margin-top: -30px; margin-left: -30px; display: inline-block; width: 60px; height: 60px; line-height: 60px; background-color: ${bgPlay}; border-radius: ${radPlay}; border: ${borderPlay}; text-align: center; box-shadow: 0 4px 10px rgba(0,0,0,0.3); vertical-align: middle;">
-                          <img src="https://img.icons8.com/ios-glyphs/60/ffffff/play.png" width="30" height="30" style="display: inline-block; margin-top: 15px; border: 0; outline: none; text-decoration: none; vertical-align: top;" alt="Play" />
+                          <svg width="30" height="30" viewBox="0 0 24 24" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" style="display: inline-block; margin-top: 15px; border: 0; outline: none; vertical-align: top;"><path d="M8 5v14l11-7z"/></svg>
                         </span>
                       </a>
                     </td>
